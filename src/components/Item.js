3c161da2cdd67787ac58f53a9e77ab00d1e32828
Martin Cueva <cuevabros@hotmail.com>
2022-09-01 23:25:50 -0300

    Catálogo con MAPS y Promises + Contador con botón

--- src/components/Item.js
+++ src/components/Item.js
@@ -29,7 +29,6 @@ const Item = ( {codigo, marca, tipo, precio, estado, stock, img} ) => {
       <div className="card w-96 bg-base-100 shadow-xl px-6 py-6">
           <strong>{tipo}</strong>
           <img className= "imagen" src={img} alt="Imagen producto"/>
-          <h6>{tipo} </h6>
           <h6>Marca: {marca} </h6>
           <h6>Stock: {stock} </h6>
           <h6>Precio: {precio}</h6>
