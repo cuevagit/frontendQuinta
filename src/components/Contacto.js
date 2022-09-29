9a3ca1e14b50cc84f3a6223cacd6e12d07b6401e
Martin Cueva <cuevabros@hotmail.com>
2022-09-29 19:19:38 -0300

    Proyecto Final

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -2,14 +2,17 @@ function Contacto(){
 
     //Datos de contacto
     return(
+        <>
         <div className="contacto">
-        <p className="card w-96 bg-base-100 shadow-xl px-6 py-6 textoContacto">      
+        <p className="w-20 bg-base-50 shadow-xl px-2 py-30 textoContacto">      
             <br></br>      
             <h2><strong>Correo electrónico: cuevabros@gmail.com</strong></h2>
             <br></br>
-            <h2><strong>Teléfono: (0345) 154941473 </strong></h2>
+            <h2><strong>Teléfono: (0345) 154941473 </strong></h2> 
         </p>
-        </div>
+        </div> 
+        <br></br>
+        </>
     )
 }
 
